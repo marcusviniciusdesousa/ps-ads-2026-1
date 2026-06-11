c26a3950d04a61533966eb22eebd3675d2df91c2
marcus291005@gmail.com <130625110+marcusviniciusdesousa@users.noreply.github.com>
2026-06-11 12:49:19 +0000

    Mudança no nome no Rodapé

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -25,7 +25,7 @@ export default function FooterBar() {
           }
         }}
       >
-        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:marcus291005@gmail.com">Marcus Vinicius de Sousa Alves</a>
       </Typography>
     </Box>
  </>
